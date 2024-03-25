4b313a7b0c85bc742ce7485b846023676ed48c46
unknown <sanjeev_jha77@rediffmail.com>
2024-03-25 08:03:47 +0530

    first commit

---
+++ src/main/java/stcom/com/consumertech/ConsumertechApplication.java
@@ -0,0 +1,13 @@
+package stcom.com.consumertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumertechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumertechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stcom/com/consumertech/ConsumertechApplicationTests.java
@@ -0,0 +1,13 @@
+package stcom.com.consumertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumertechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
